42ea172863af908c18ef3e29664ef8e7b6d3f010
thweattm <thweattm@oregonstate.edu>
2018-09-17 10:32:18 -0600

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
  connectionLimit : 10,
- host : 'classmysql.engr.oregonstate.edu',
- user : 'cs361_thweattm',
- password : '7257',
- database : 'cs361_thweattm'
+ host : '---',
+ user : '---',
+ password : '---',
+ database : '---'
 });
 
 module.exports.pool = pool;
